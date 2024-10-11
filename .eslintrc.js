3b115958e11a571079c6fd1343cd5fa5edb66c42
oseajicoy <oseadevteam@gmail.com>
2024-10-11 08:24:24 +0800

    feat: init style configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/*
+ * @Description:
+ * @Author: F-Stone
+ * @LastEditTime: 2022-08-31 15:15:36
+ */
+
+module.exports = {
+    extends: ["plugin:@stone/base/ts"],
+    parserOptions: {
+        requireConfigFile: false,
+    },
+    globals: { Generator: "readonly" },
+};
