f6025c714c27f4078049176dafdc4df97e77f8e3
oseajicoy <oseadevteam@gmail.com>
2024-10-14 13:46:58 +0800

    feat: init linting configs

---
+++ local-project/wetalk-home/.eslintrc.js
@@ -0,0 +1,19 @@
+/*
+ * @Description:
+ * @Author: F-Stone
+ * @LastEditTime: 2022-08-31 17:24:26
+ */
+
+module.exports = {
+    extends: ["plugin:base/vue"],
+    parserOptions: {
+        requireConfigFile: false,
+    },
+    rules: {
+        "@typescript-eslint/no-var-requires": 0,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "vue/multi-word-component-names": 0,
+    },
+    globals: { Generator: "readonly", $: "readonly", gsap: "readonly" },
+    ignorePatterns: ["public/**/*.js", "static/**/*.js", ".nuxt/**/*.*"],
+};
