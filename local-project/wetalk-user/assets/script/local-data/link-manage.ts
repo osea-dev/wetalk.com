6b16e0a074593c82f2c3daa4dfc66ed9d15b03e3
oseajicoy <oseadevteam@gmail.com>
2024-10-14 11:22:04 +0800

    feat: init nav data local

---
+++ local-project/wetalk-user/assets/script/local-data/link-manage.ts
@@ -0,0 +1,25 @@
+/*
+ * @Description: wetalk-user 链接管理
+ * @Author: F-Stone
+ * @LastEditTime: 2022-12-06 12:12:19
+ */
+export default {
+    index: "/",
+    login: "/login",
+    register: "/register",
+    forgotPW: "/forgot-pw",
+    course: "/course",
+    report: "/report",
+    order: "/order",
+    orderCreate: "/order/create",
+    orderPay: "/order/pay",
+    courseLesson: "/course/lesson",
+    courseHomework: "/course/homework",
+    schedule: "/schedule",
+    setting: "/setting",
+    notice: "/notice",
+    orderInfo: "/order/info",
+    policy: "/page/policy",
+    service: "/page/service",
+    redirect: "/redirect",
+};
---
+++ local-project/wetalk-user/assets/script/local-data/site-nav-list.ts
@@ -0,0 +1,20 @@
+import linkManage from "@/assets/script/local-data/link-manage";
+
+export const siteNavList = [
+    {
+        link: linkManage.index,
+        title: "KEY_NAV.CENTER",
+    },
+    {
+        link: linkManage.course,
+        title: "KEY_NAV.COURSE",
+    },
+    {
+        link: linkManage.schedule,
+        title: "KEY_NAV.SCHEDULE",
+    },
+    {
+        link: linkManage.report,
+        title: "KEY_NAV.REPORT",
+    },
+];
