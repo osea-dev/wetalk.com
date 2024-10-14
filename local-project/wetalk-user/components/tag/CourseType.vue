926adc8492f9fe62e563a1d2340f77025accb72f
cortejojicoy <johnjercai.cortejo@lspu.edu.ph>
2024-10-14 10:54:11 +0800

    feat: init tag components

---
+++ local-project/wetalk-user/components/tag/CourseType.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+        :class="$style['type-tag']"
+        class="inline-flex items-center justify-center"
+    >
+        <i class="ic" :class="info.icon"></i>
+        <span class="text">{{ info.name }}</span>
+    </div>
+</template>
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const prop = defineProps<{ type: "小班课" }>();
+
+const info = computed(() => {
+    return {
+        小班课: {
+            icon: "iconfont icon-xiaobanke",
+            name: "小班课",
+        },
+    }[prop.type];
+});
+</script>
+<style lang="scss" module>
+.type-tag {
+    font-size: 13px;
+    font-weight: 700;
+    line-height: em(20px, 13px);
+
+    padding: 8px 20px;
+
+    color: #f8af00;
+    border-radius: 18px;
+    background: rgb(248 175 0 / 0.2);
+    :global(.ic) {
+        font-size: 14px;
+
+        margin-right: 4px;
+    }
+}
+</style>
---
+++ local-project/wetalk-user/components/tag/Level.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 等级标签
+ * @Author: F-Stone
+ * @LastEditTime: 2022-11-18 22:18:22
+-->
+<template>
+    <div :class="$style['tag-level']">
+        <slot />
+    </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" module>
+.tag-level {
+    font-family: "Inter";
+    font-size: 13px;
+    font-weight: 700;
+    line-height: em(16px, 13px);
+
+    padding: 8px 20px;
+
+    color: #f8af00;
+    border-radius: 16px;
+    background: rgb(248 175 0 / 0.2);
+}
+</style>
